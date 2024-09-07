25779093fbe063be794efcb169344d9e164ad605
Arjun <arjun8107@gmail.com>
2024-09-07 20:18:57 +0530

    refractor to objects

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,11 +1,12 @@
 import Part from "./Part";
 
-export default function Content({ parts, exercises }) {
+export default function Content({ parts }) {
+  let { part1, part2, part3 } = parts;
   return (
     <div>
-      <Part part={parts.part1} exercises={exercises.exercises1} />
-      <Part part={parts.part2} exercises={exercises.exercises2} />
-      <Part part={parts.part3} exercises={exercises.exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   );
 }
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,4 +1,8 @@
-export default function Total({ exercises }) {
-  let { exercises1, exercises2, exercises3 } = exercises;
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+export default function Total({ parts }) {
+  let { part1, part2, part3 } = parts;
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  );
 }
