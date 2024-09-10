9a56597ba177b1c22b12e054d30cf4aa55e3c22a
Arjun <arjun8107@gmail.com>
2024-09-11 00:10:47 +0530

    feat. show

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,50 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const CountryView = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>
+        {" "}
+        Capital: <span>{country.capital.join(",")}</span>
+      </p>
+      <p>
+        Area: <span>{country.area}</span>
+      </p>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(country.languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} />
+    </div>
+  );
+};
+
+const ListItem = ({ country }) => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <p>
+        {country.name.common} {"  "}
+        <button onClick={() => setToggle((t) => !t)}>
+          {toggle ? "Hide" : "Show"}
+        </button>
+      </p>
+      {toggle ? <CountryView country={country} /> : <></>}
+    </div>
+  );
+};
 const Countries = ({ countries }) => {
   if (countries.length > 10)
     return <p>Too many matches, specify another filter</p>;
   if (countries.length !== 1)
     return countries.map((country) => (
-      <p key={country.ccn3}>{country.name.common}</p>
+      <ListItem country={country} key={country.ccn3} />
     ));
-  if (countries.length === 1) {
-    const country = countries[0];
-    return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <p>
-          {" "}
-          Capital: <span>{country.capital.join(",")}</span>
-        </p>
-        <p>
-          Area: <span>{country.area}</span>
-        </p>
-        <h2>Languages</h2>
-        <ul>
-          {Object.values(country.languages).map((language) => (
-            <li key={language}>{language}</li>
-          ))}
-        </ul>
-        <img src={country.flags.png} />
-      </div>
-    );
-  }
+  if (countries.length === 1) return <CountryView country={countries[0]} />;
 };
 function App() {
   const [countries, setCountries] = useState([]);
