50bf4a37630e1dc242e930c5db368412f83d13a4
Arjun <arjun8107@gmail.com>
2024-09-10 21:36:58 +0530

    Component

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,79 @@
 import { useState } from "react";
 
+const Filter = ({ search, setSearch }) => {
+  return (
+    <p>
+      filter shown with{" "}
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+    </p>
+  );
+};
+
+const PersonForm = ({
+  persons,
+  setPersons,
+  newName,
+  setNewName,
+  newPhone,
+  setNewPhone,
+}) => {
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (persons.some((person) => person.name === newName)) {
+          alert(`${newName} is already in the phonebook!`);
+          return;
+        }
+        setPersons((persons) => [
+          ...persons,
+          { name: newName, number: newPhone, id: persons.length },
+        ]);
+        setNewName("");
+        setNewPhone("");
+      }}
+    >
+      <div>
+        name:{" "}
+        <input
+          value={newName}
+          onChange={(e) => {
+            setNewName(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        phone:{" "}
+        <input
+          value={newPhone}
+          onChange={(e) => {
+            setNewPhone(e.target.value);
+          }}
+        />
+      </div>
+
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <table>
+      {persons.map((person) => (
+        <tbody key={person.id}>
+          <tr>
+            <td>{person.name}</td>
+            <td>{person.number}</td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -19,60 +93,18 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <p>
-        filter shown with{" "}
-        <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      </p>
+      <Filter search={search} setSearch={setSearch} />
       <h2>Add new</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (persons.some((person) => person.name === newName)) {
-            alert(`${newName} is already in the phonebook!`);
-            return;
-          }
-          setPersons((persons) => [
-            ...persons,
-            { name: newName, phone: newPhone, id: persons.length },
-          ]);
-          setNewName("");
-          setNewPhone("");
-        }}
-      >
-        <div>
-          name:{" "}
-          <input
-            value={newName}
-            onChange={(e) => {
-              setNewName(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          phone:{" "}
-          <input
-            value={newPhone}
-            onChange={(e) => {
-              setNewPhone(e.target.value);
-            }}
-          />
-        </div>
-
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newPhone={newPhone}
+        setNewPhone={setNewPhone}
+      />
       <h2>Numbers</h2>
-      <table>
-        {filteredPersons.map((person) => (
-          <tbody key={person.name}>
-            <tr>
-              <td>{person.name}</td>
-              <td>{person.phone}</td>
-            </tr>
-          </tbody>
-        ))}
-      </table>
+      <Persons persons={filteredPersons} />
     </div>
   );
 };
